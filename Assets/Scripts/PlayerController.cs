dd07d610a2d644d14146540a487610108463adb6
Ivan Kelber <ivankelber@gmail.com>
2020-05-17 10:33:35 -0400

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : RaycastController
+{
+
+    // Update is called once per frame
+    public void Move(Vector2 moveAmount)
+    {
+        UpdateRaycastOrigins();
+        if(moveAmount.x != 0) {
+            HorizontalCollisions(ref moveAmount);
+        }
+        if(moveAmount.y != 0) {
+            VerticalCollisions(ref moveAmount);
+        }
+        transform.Translate(moveAmount);
+    }
+
+     void HorizontalCollisions(ref Vector2 moveAmount)
+    {
+        float directionX = Mathf.Sign(moveAmount.x);
+        float rayLength = Mathf.Abs(moveAmount.x) + skinWidth;
+
+        if (Mathf.Abs(moveAmount.x) < skinWidth)
+        {
+            rayLength = 2 * skinWidth;
+        }
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                moveAmount.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+    void VerticalCollisions(ref Vector2 moveAmount)
+    {
+        float directionY = Mathf.Sign(moveAmount.y);
+        float rayLength = Mathf.Abs(moveAmount.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + moveAmount.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                moveAmount.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+               
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[RequireComponent(typeof(PlayerController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+    private PlayerController controller;
+    private Animator animator;
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 velocity = playerInput * speed;
+
+        velocity = Vector2.ClampMagnitude(velocity, speed) * Time.deltaTime;
+        ApplyAnimation(velocity);
+        controller.Move(velocity);
+    }
+
+    void ApplyAnimation(Vector2 velocity) {
+        int directionX = Math.Sign(velocity.x);
+        int directionY = Math.Sign(velocity.y);
+        if(animator != null) {
+            animator.SetInteger("DirectionX", directionX);
+            animator.SetInteger("DirectionY", directionY);
+        } else {
+            Debug.Log("Player Animator is not set");
+        }
+    }
+}
