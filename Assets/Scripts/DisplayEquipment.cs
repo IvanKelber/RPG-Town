6f66e7d55b50fe64f74c0aaa7c0f471fd3567f6a
Ivan Kelber <ivankelber@gmail.com>
2020-05-24 17:39:28 -0400

    basic stats ui

--- Assets/Scripts/DisplayEquipment.cs
+++ Assets/Scripts/DisplayEquipment.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class DisplayEquipment : MonoBehaviour
 {
     [SerializeField]
     Equipment equipment;
+    [SerializeField]
+    DerivedStats stats;
 
     private int displayAlpha = 0;
     private CanvasGroup cg;
 
+
+    [SerializeField]
+    private TMP_Text playerStrength;
+    [SerializeField]
+    private TMP_Text playerAgility;
+    [SerializeField]
+    private TMP_Text playerDefense;
+    [SerializeField]
+    private TMP_Text playerConstitution;
+    [SerializeField]
+    private TMP_Text playerWisdom;
+    [SerializeField]
+    private TMP_Text playerLck;
 
     // [SerializeField]
     // private GameObject itemSlotsParent;
@@ -26,7 +41,20 @@ public class DisplayEquipment : MonoBehaviour
     }
 
     public void UpdateUI() {
+        
+    }
 
+    public void Update() {
+        UpdateStats();
+    }
+
+    public void UpdateStats() {
+        playerStrength.text = "Str: " + stats.Strength;
+        playerAgility.text = "Agi: " + stats.Agility;
+        playerDefense.text = "Def: " + stats.Defense;
+        playerConstitution.text = "Con: " + stats.Constitution;
+        playerWisdom.text = "Wis: " + stats.Wisdom;
+        playerLck.text = "Lck: " + stats.Luck;
     }
 
     public void OnToggleDisplay() {
