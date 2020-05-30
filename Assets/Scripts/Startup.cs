a84dae7ee53cb0cc00b8c6dd9cbbda115fb3a2af
Ivan Kelber <ivankelber@gmail.com>
2020-05-30 18:51:11 -0400

    added startup script

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startup : MonoBehaviour
+{
+    public SceneInfo[] scenes;
+
+    public ItemCollection playerInventory;
+    public Equipment playerEquipment;
+    public PrimaryStats playerBaseStats;
+
+    public SceneLoader townLoader;
+
+    public void NewStart() {
+        foreach(SceneInfo scene in scenes) {
+            scene.SetToDefault();
+        }
+        playerInventory.Clear();
+        playerEquipment.Clear();
+        playerBaseStats.Default();
+        townLoader.Load();
+    }
+}
